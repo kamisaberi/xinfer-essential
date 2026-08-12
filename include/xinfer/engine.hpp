6aa0d9765234855cc31a70904cfb850d0e23034a
= <kamisaberi@gmail.com>
2026-08-13 02:20:06 +0330

    initlal release

---
+++ include/xinfer/engine.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <memory>
+#include "target.hpp"
+#include "backend.hpp"
+
+namespace xinfer {
+
+class Engine {
+public:
+    explicit Engine(Target target);
+    ~Engine() = default;
+
+    void load_model(const std::string& model_path);
+    void infer();
+
+    Tensor& get_input_tensor(const std::string& name);
+    Tensor& get_output_tensor(const std::string& name);
+
+    Target target() const { return target_; }
+
+private:
+    Target target_;
+    std::unique_ptr<Backend> backend_;
+};
+
+} // namespace xinfer
