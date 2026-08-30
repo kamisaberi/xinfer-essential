48879ba499bea8f427b19412d10b3f10dc081244
kamran saberifard <kamisaberi@gmail.com>
2026-08-30 04:14:52 +0330

    some  bugs fixed

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kami/xinfer-essential
-# Build directory: /home/kami/xinfer-essential/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
