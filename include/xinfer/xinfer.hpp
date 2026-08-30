08e056eef6fde3eb2657867e3ebc0759c1de0ac9
kamran saberifard <kamisaberi@gmail.com>
2026-08-30 04:13:56 +0330

    some  bugs fixed

--- include/xinfer/xinfer.hpp
+++ include/xinfer/xinfer.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+
+#include "xinfer/export.hpp"
 #include "xinfer/target.hpp"
 #include "xinfer/memory.hpp"
 #include "xinfer/tensor.hpp"
 #include "xinfer/backend.hpp"
 #include "xinfer/engine.hpp"
+#include "xinfer/hub.hpp"        // <--- ADD THIS LINE
