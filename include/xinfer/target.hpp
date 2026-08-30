8fea4e5704b92a5c716aa74a9acddaf756c9f256
kamran saberifard <kamisaberi@gmail.com>
2026-08-31 00:06:14 +0330

    some files changed

--- include/xinfer/target.hpp
+++ include/xinfer/target.hpp
@@ -1,15 +1,29 @@
 #pragma once
+
+#include "export.hpp"
 #include <string>
 
 namespace xinfer {
 
-enum class Target {
-    TensorRT,
-    OpenVINO,
-    RKNN
+enum class XINFER_API Target {
+    TensorRT,    // NVIDIA (RTX GPUs, Jetson Orin) [.engine]
+    OpenVINO,    // Intel (Core Ultra NPU, Arc GPU, x86 CPU) [.xml/.bin]
+    RKNN,        // Rockchip (RK3588, RK3568, RV1126) [.rknn]
+    VitisAI,     // AMD / Xilinx (Kria SOM, Zynq MPSoC, Versal) [.xmodel]
+    QNN,         // Qualcomm (Snapdragon 8 Gen 2/3, HTP) [.bin/.so]
+    CoreML,      // Apple (M1/M2/M3, A-Series) [.mlmodelc]
+    RyzenAI,     // AMD (Ryzen 7040/8040 IPU) [.onnx]
+    NeuroPilot,  // MediaTek (Genio 1200, Dimensity) [.dla/.pte]
+    Hailo,       // Hailo (Hailo-8 / 8L / 10) [.hef]
+    CVFlow,      // Ambarella (CV2, CV3, CV5) [.cavalry]
+    ENN,         // Samsung Exynos (Eden NPU) [.nnc]
+    EdgeTPU,     // Google Coral (USB, M.2, Dev Board) [.tflite]
+    FPGA_AI,     // Intel FPGA (Agilex, Stratix 10) [.aocx]
+    VectorBlox,  // Microchip (PolarFire FPGA) [.blob]
+    SensAI       // Lattice (CrossLink-NX) [.bin]
 };
 
-enum class DataType {
+enum class XINFER_API DataType {
     Float32,
     Float16,
     Int32,
@@ -19,10 +33,22 @@ enum class DataType {
 
 inline std::string target_to_string(Target target) {
     switch (target) {
-        case Target::TensorRT: return "NVIDIA TensorRT";
-        case Target::OpenVINO: return "Intel OpenVINO";
-        case Target::RKNN:     return "Rockchip RKNN";
-        default:               return "Unknown Target";
+        case Target::TensorRT:   return "NVIDIA TensorRT";
+        case Target::OpenVINO:   return "Intel OpenVINO";
+        case Target::RKNN:       return "Rockchip RKNN";
+        case Target::VitisAI:    return "AMD/Xilinx Vitis AI";
+        case Target::QNN:        return "Qualcomm QNN";
+        case Target::CoreML:     return "Apple CoreML/Metal";
+        case Target::RyzenAI:    return "AMD Ryzen AI";
+        case Target::NeuroPilot: return "MediaTek NeuroPilot";
+        case Target::Hailo:      return "Hailo HailoRT";
+        case Target::CVFlow:     return "Ambarella CVFlow";
+        case Target::ENN:        return "Samsung ENN";
+        case Target::EdgeTPU:    return "Google Coral Edge TPU";
+        case Target::FPGA_AI:    return "Intel FPGA AI Suite";
+        case Target::VectorBlox: return "Microchip VectorBlox";
+        case Target::SensAI:     return "Lattice sensAI";
+        default:                 return "Unknown Target";
     }
 }
 
