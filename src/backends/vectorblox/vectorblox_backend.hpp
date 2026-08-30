1a126bc2211bfd6e27ac707c86bce13cd02842da
kamran saberifard <kamisaberi@gmail.com>
2026-08-31 01:25:27 +0330

    some files changed

---
+++ src/backends/vectorblox/vectorblox_backend.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "xinfer/backend.hpp"
+#include <unordered_map>
+
+namespace xinfer {
+
+class VectorBloxBackend : public Backend {
+public:
+    VectorBloxBackend();
+    ~VectorBloxBackend() override = default;
+
+    void load_model(const std::string& model_path) override;
+    void infer() override;
+
+    Tensor& get_input_tensor(const std::string& name) override;
+    Tensor& get_output_tensor(const std::string& name) override;
+    void set_stream(void* stream_ptr) override;
+
+private:
+    std::unordered_map<std::string, Tensor> input_tensors_;
+    std::unordered_map<std::string, Tensor> output_tensors_;
+};
+
+} // namespace xinfer
