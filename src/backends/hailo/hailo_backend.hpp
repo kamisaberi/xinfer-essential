1c2dacf636cb68bbdff7e7f1a1c1447df508388a
kamran saberifard <kamisaberi@gmail.com>
2026-08-31 01:10:47 +0330

    some files changed

--- src/backends/hailo/hailo_backend.hpp
+++ src/backends/hailo/hailo_backend.hpp
@@ -2,11 +2,15 @@
 #include "xinfer/backend.hpp"
 #include <unordered_map>
 
+#ifdef XINFER_ENABLE_HAILO
+#include "hailo/hailort.hpp"
+#endif
+
 namespace xinfer {
 
 class HailoBackend : public Backend {
 public:
-    HailoBackend() = default;
+    HailoBackend();
     ~HailoBackend() override = default;
 
     void load_model(const std::string& model_path) override;
@@ -17,8 +21,12 @@ public:
     void set_stream(void* stream_ptr) override;
 
 private:
+#ifdef XINFER_ENABLE_HAILO
+    std::unique_ptr<hailort::VDevice> vdevice_;
+    std::unique_ptr<hailort::InferModel> infer_model_;
     std::unordered_map<std::string, Tensor> input_tensors_;
     std::unordered_map<std::string, Tensor> output_tensors_;
+#endif
 };
 
 } // namespace xinfer
