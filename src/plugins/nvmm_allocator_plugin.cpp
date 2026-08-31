552d8580bfacdf3bd989d2e40d830cbc2b3da346
kamran saberifard <kamisaberi@gmail.com>
2026-08-31 23:28:39 +0330

    some files changed

---
+++ src/plugins/nvmm_allocator_plugin.cpp
@@ -0,0 +1,28 @@
+#include "xinfer/plugin.hpp"
+#include <iostream>
+
+class NVMMAllocatorPlugin : public xinfer::plugin::IInferencePlugin {
+public:
+    const char* get_name() const override { return "NVIDIA_NVMM_ZeroCopy_Allocator"; }
+    const char* get_version() const override { return "1.0.0"; }
+    xinfer::plugin::PluginType get_type() const override { return xinfer::plugin::PluginType::MemoryAllocator; }
+
+    bool initialize(const std::string& config_json) override {
+        std::cout << "[xInfer Plugin: Allocator] NVIDIA Jetson NVMM (NvBuffer) Zero-Copy Allocator Initialized." << std::endl;
+        return true;
+    }
+
+    bool execute(xinfer::Tensor& input, xinfer::Tensor& output, void* stream) override {
+        std::cout << "[xInfer Plugin: Allocator] Wrapped NvBuffer hardware memory handle for Jetson Orin." << std::endl;
+        return true;
+    }
+
+    void shutdown() override {
+        std::cout << "[xInfer Plugin: Allocator] NVMM Allocator shut down." << std::endl;
+    }
+};
+
+extern "C" {
+    xinfer::plugin::IInferencePlugin* create_plugin() { return new NVMMAllocatorPlugin(); }
+    void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) { delete plugin; }
+}
