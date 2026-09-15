e22d1baae9c43fa64e5fe16643f579cd03291946
kamran saberifard <kamisaberi@gmail.com>
2026-09-16 01:53:15 +0330

    some plugins updated

--- examples/07_plugin_vision_yolo.cpp
+++ examples/07_plugin_vision_yolo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <xinfer/xinfer.hpp>
+#include <cstdint>
 
 int main() {
     std::cout << "==========================================================" << std::endl;
