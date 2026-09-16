1decad19dfa61557350557b9778bf01e5e499b0c
kamran saberifard <kamisaberi@gmail.com>
2026-09-16 13:23:32 +0330

    some bugs fixed

--- src/plugins/onevpl_decoder_plugin.cpp
+++ src/plugins/onevpl_decoder_plugin.cpp
@@ -23,6 +23,6 @@ public:
 };
 
 extern "C" {
-    xinfer::plugin::IInferencePlugin* create_plugin() { return new OneVPLDecoderPlugin(); }
-    void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) { delete plugin; }
+    XINFER_API xinfer::plugin::IInferencePlugin* create_plugin() { return new OneVPLDecoderPlugin(); }
+    XINFER_API void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) { delete plugin; }
 }
