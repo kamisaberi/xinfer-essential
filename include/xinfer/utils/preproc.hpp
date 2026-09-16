dd8ad1e5f934263347f0aad9b9c41b28bd64d39c
kamran saberifard <kamisaberi@gmail.com>
2026-09-16 13:07:06 +0330

    some bugs fixed

--- include/xinfer/utils/preproc.hpp
+++ include/xinfer/utils/preproc.hpp
@@ -6,13 +6,13 @@
 
 namespace xinfer::utils {
 
-struct BoundingBox {
+struct XINFER_API  BoundingBox {
     float x1, y1, x2, y2;
     float confidence;
     int class_id;
 };
 
-void cpu_letterbox_normalize(
+XINFER_API void cpu_letterbox_normalize(
     const uint8_t* src_bgr, int src_w, int src_h,
     float* dst_float, int target_w, int target_h
 );
