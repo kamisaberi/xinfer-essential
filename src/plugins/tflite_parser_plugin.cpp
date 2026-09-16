8b7be2fc2656aa26671a05b4e5a851eb31202c30
kamran saberifard <kamisaberi@gmail.com>
2026-09-16 13:24:52 +0330

    some bugs fixed

--- src/plugins/tflite_parser_plugin.cpp
+++ src/plugins/tflite_parser_plugin.cpp
@@ -23,6 +23,6 @@ public:
 };
 
 extern "C" {
-    xinfer::plugin::IInferencePlugin* create_plugin() { return new TFLiteParserPlugin(); }
-    void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) { delete plugin; }
+    XINFER_API xinfer::plugin::IInferencePlugin* create_plugin() { return new TFLiteParserPlugin(); }
+    XINFER_API void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) { delete plugin; }
 }
