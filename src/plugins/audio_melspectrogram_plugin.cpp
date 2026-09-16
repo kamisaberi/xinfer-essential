bea2ec0e6f76c41a5c19c3cb627528bd356c8fe1
kamran saberifard <kamisaberi@gmail.com>
2026-09-16 13:15:54 +0330

    some bugs fixed

--- src/plugins/audio_melspectrogram_plugin.cpp
+++ src/plugins/audio_melspectrogram_plugin.cpp
@@ -47,10 +47,10 @@ private:
 };
 
 extern "C" {
-    xinfer::plugin::IInferencePlugin* create_plugin() {
+    XINFER_API xinfer::plugin::IInferencePlugin* create_plugin() {
         return new AudioMelSpectrogramPlugin();
     }
-    void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) {
+    XINFER_API void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) {
         delete plugin;
     }
 }
