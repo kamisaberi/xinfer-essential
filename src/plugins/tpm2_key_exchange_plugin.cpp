69b6f8c8b26210c64009d03f1d1b0545d3eccc11
kamran saberifard <kamisaberi@gmail.com>
2026-09-16 13:25:10 +0330

    some bugs fixed

--- src/plugins/tpm2_key_exchange_plugin.cpp
+++ src/plugins/tpm2_key_exchange_plugin.cpp
@@ -37,10 +37,10 @@ private:
 };
 
 extern "C" {
-    xinfer::plugin::IInferencePlugin* create_plugin() {
+    XINFER_API xinfer::plugin::IInferencePlugin* create_plugin() {
         return new TPM2HardwareKeyExchangePlugin();
     }
-    void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) {
+    XINFER_API void destroy_plugin(xinfer::plugin::IInferencePlugin* plugin) {
         delete plugin;
     }
 }
